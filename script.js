ae7bdc96c6413224aec94dc071b5ec1c7c1ddae8
Alvin Hart <105436283+greenzombie123@users.noreply.github.com>
2024-06-26 11:23:31 +0900

    Click outside of modal or click on buttons to close modal

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 var idCounter = 1;
 var modal = document.querySelector('dialog');
+var viewBookModal = createViewBookModal();
+// modal.addEventListener("click", ()=>modal.close())
+// modal.addEventListener('click', (e)=>e.stopPropagation())
 var ReadingStatus;
 (function (ReadingStatus) {
     ReadingStatus[ReadingStatus["NotStarted"] = 0] = "NotStarted";
@@ -7,10 +10,11 @@ var ReadingStatus;
     ReadingStatus[ReadingStatus["Finished"] = 2] = "Finished";
 })(ReadingStatus || (ReadingStatus = {}));
 var books = [
-    { title: "WOw", pageNum: 12, author: "Brian Lop", image: null, status: ReadingStatus.Finished, id: idCounter }
+    { title: "We are light of the world!", pageNum: 12, author: "Brian Lop", image: null, status: ReadingStatus.Finished, id: idCounter },
+    { title: "Star and Spills", pageNum: 122, author: "Jeff Loppy", image: null, status: ReadingStatus.NotStarted, id: incrementIDCounter() }
 ];
 function incrementIDCounter() {
-    idCounter = +idCounter;
+    idCounter = ++idCounter;
     return idCounter;
 }
 function Book(title, pageNum, author, image, status) {
@@ -28,6 +32,30 @@ Book.prototype.setValues = function (newValues) {
     this.image = newValues.image;
     this.status = newValues.status;
 };
+function createViewBookModal() {
+    var viewBookModal = document.createElement('form');
+    viewBookModal.method = "dialog";
+    viewBookModal.className = "viewBookModal";
+    var boxes = [];
+    var classNames = ['titleBox', 'authorBox', 'pageBox', 'statusBox'];
+    for (var index = 0; index < 4; index++) {
+        boxes.push(document.createElement('p'));
+        boxes[index].className = classNames[index];
+        viewBookModal.appendChild(boxes[index]);
+    }
+    var buttons = document.createElement('div');
+    buttons.className = 'buttons';
+    var editButton = document.createElement('button');
+    editButton.className = "editButton";
+    editButton.textContent = "Edit";
+    var cancelButton = document.createElement('button');
+    cancelButton.className = "cancelButton";
+    cancelButton.textContent = "Cancel";
+    buttons.appendChild(editButton);
+    buttons.appendChild(cancelButton);
+    viewBookModal.appendChild(buttons);
+    return viewBookModal;
+}
 function renderBooks(books) {
     var booksContainer = document.querySelector('.booksContainer');
     books.forEach(function (book) {
@@ -37,12 +65,28 @@ function renderBooks(books) {
         bookImage.dataset.bookId = book.id.toString();
         bookImage.addEventListener("click", function (e) {
             var bookInfo = findBook(e);
-            console.log(bookInfo);
+            seeBookInfo(bookInfo, viewBookModal, modal);
         });
         booksContainer.appendChild(bookImage);
     });
 }
-function seeBookInfo(book) {
+function cancelModalOutside(e) {
+    if (e.target === modal) {
+        modal.close();
+    }
+}
+function seeBookInfo(book, viewBookModal, modal) {
+    var _a;
+    if (modal.hasChildNodes()) {
+        (_a = modal.firstChild) === null || _a === void 0 ? void 0 : _a.replaceWith(viewBookModal);
+    }
+    else
+        modal.appendChild(viewBookModal);
+    viewBookModal.children[0].textContent = book.title;
+    viewBookModal.children[1].textContent = 'Author: ' + book.author;
+    viewBookModal.children[2].textContent = "".concat(book.pageNum, " pages");
+    viewBookModal.children[3].textContent = 'Status: ' + (book.status === ReadingStatus.NotStarted ? "Haven't read yet" : book.status === ReadingStatus.Reading ? "Reading Now" : "Finished");
+    modal.showModal();
 }
 function findBook(e) {
     // Turn the string dataset value to a number
@@ -50,6 +94,7 @@ function findBook(e) {
     return books.find(function (book) { return book.id === bookId; });
 }
 function initialize() {
+    modal.addEventListener("click", cancelModalOutside);
     renderBooks(books);
 }
 initialize();
--- script.ts
+++ script.ts
@@ -1,5 +1,9 @@
 let idCounter = 1
 const modal = document.querySelector('dialog')!
+const viewBookModal: HTMLFormElement = createViewBookModal()
+
+// modal.addEventListener("click", ()=>modal.close())
+// modal.addEventListener('click', (e)=>e.stopPropagation())
 
 enum ReadingStatus {
     NotStarted,
@@ -13,15 +17,16 @@ interface Book {
     author: string;
     image: string | null;
     status: ReadingStatus;
-    id:number
+    id: number
 }
 
 const books: Book[] = [
-    { title: "WOw", pageNum: 12, author: "Brian Lop", image: null, status: ReadingStatus.Finished, id:idCounter }
+    { title: "We are light of the world!", pageNum: 12, author: "Brian Lop", image: null, status: ReadingStatus.Finished, id: idCounter }, 
+    { title: "Star and Spills", pageNum: 122, author: "Jeff Loppy", image: null, status: ReadingStatus.NotStarted, id: incrementIDCounter() }
 ]
 
-function incrementIDCounter(){
-    idCounter = +idCounter
+function incrementIDCounter() {
+    idCounter = ++idCounter
     return idCounter
 }
 
@@ -42,6 +47,37 @@ Book.prototype.setValues = function (newValues: Book) {
     this.status = newValues.status
 }
 
+function createViewBookModal() {
+
+    const viewBookModal = document.createElement('form')
+    viewBookModal.method = "dialog"
+    viewBookModal.className = "viewBookModal"
+    const boxes: HTMLParagraphElement[] = []
+    const classNames: string[] = ['titleBox', 'authorBox', 'pageBox', 'statusBox']
+
+    for (let index = 0; index < 4; index++) {
+        boxes.push(document.createElement('p'))
+        boxes[index].className = classNames[index]
+        viewBookModal.appendChild(boxes[index])
+    }
+
+    const buttons = document.createElement('div')
+    buttons.className = 'buttons'
+
+    const editButton = document.createElement('button')
+    editButton.className = "editButton"
+    editButton.textContent = "Edit"
+    const cancelButton = document.createElement('button')
+    cancelButton.className = "cancelButton"
+    cancelButton.textContent = "Cancel"
+
+    buttons.appendChild(editButton)
+    buttons.appendChild(cancelButton)
+    viewBookModal.appendChild(buttons)
+
+    return viewBookModal
+}
+
 function renderBooks(books: Book[]) {
     const booksContainer = document.querySelector('.booksContainer')!
 
@@ -50,25 +86,44 @@ function renderBooks(books: Book[]) {
         bookImage.className = "bookImage"
         bookImage.src = book.image || "./assets/bookcover.jpg"
         bookImage.dataset.bookId = book.id.toString()
-        bookImage.addEventListener("click", (e)=>{
-            const bookInfo = findBook(e)
-            console.log(bookInfo)
+        bookImage.addEventListener("click", (e) => {
+            const bookInfo = findBook(e)!
+            seeBookInfo(bookInfo, viewBookModal, modal)
         })
         booksContainer.appendChild(bookImage)
     })
 }
 
-function seeBookInfo(book:Book){
-
+function cancelModalOutside(e: Event) {
+    if (e.target === modal) {
+        modal.close()
+    }
 }
 
-function findBook(e:Event){
+function seeBookInfo(book: Book, viewBookModal: HTMLFormElement, modal: HTMLDialogElement) {
+    if (modal.hasChildNodes()) {
+        modal.firstChild?.replaceWith(viewBookModal)
+    }
+    else
+        modal.appendChild(viewBookModal)
+
+    viewBookModal.children[0].textContent = book.title
+    viewBookModal.children[1].textContent = 'Author: ' + book.author
+    viewBookModal.children[2].textContent = `${book.pageNum} pages`
+    viewBookModal.children[3].textContent = 'Status: ' + (book.status === ReadingStatus.NotStarted ? "Haven't read yet" : book.status === ReadingStatus.Reading ? "Reading Now" : "Finished")
+
+    modal.showModal()
+}
+
+function findBook(e: Event) {
     // Turn the string dataset value to a number
     const bookId = +(e.target as HTMLElement).dataset.bookId!
-    return books.find(book=>book.id === bookId)
+    return books.find(book => book.id === bookId)
 }
 
-function initialize(){
+function initialize() {
+    modal.addEventListener("click", cancelModalOutside)
+
     renderBooks(books)
 }
 
